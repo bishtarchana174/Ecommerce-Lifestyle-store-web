525161eddfc2fc81375aca75c581fa7a85883118
bishtarchana174 <68501576+bishtarchana174@users.noreply.github.com>
2021-08-21 23:30:10 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+	<div class="container">
+		<center>Copyright <span class="glyphicon glyphicon-copyright-mark"></span> Archana Bisht 
+                    | All Rights Reserved | Contact Us: +91 6399104055</center>
+	</div>
+</footer>
